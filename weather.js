ccf74b463bbd6ffe754cf2f370f62d8728c89d82
kaushal831848 <kaushalsingh8318480481@gmail.com>
2024-11-18 23:20:17 +0530

    population API

--- weather.js
+++ weather.js
@@ -8,23 +8,47 @@ function getweather(){
     if(city===""){
         weatherDetails.innerHTML="<p>Please Enter a city</p>";
         return;
+        weatherDetails.innerHTML=""
     }
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const populationApi =  ` https://countriesnow.space/api/v0.1/countries/population/cities`;
     fetch(url)
     .then((Response)=>Response.json())
     .then((data)=>{
         console.log(data);
         if(data.cod==="404"){
-            weatherDetails.innerHTML="<p>City not Found</p>"
+            weatherDetails.innerHTML +="<p>City not Found</p>"
         }
         else{
-            weatherDetails.innerHTML=`<h2>${data.name},${data.sys.country}</h2>
+            weatherDetails.innerHTML +=`<h2>${data.name},${data.sys.country}</h2>
             <p>Tempatature:${data.main.temp}</p>`;
         }
     })
-    .catch((error)=>{
-        weatherDetails.innerHTML="<p>Error</p>"  
-    })
+   
     
+    fetch(populationApi, {
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify({
+            city:city
+        })
+    })
+    .then((response) => response.json())
+    .then((populationData) => {
+        if(populationData.data && populationData.data.populationCounts && populationData.data.populationCounts.length >= 0){
+            weatherDetails.innerHTML += `<p>Population : ${populationData.data.populationCounts[0].value}</p>`;
+        }else{
+            // weatherdetails.innerHTML = <p> Population in ${city} is : ${data}
+            console.log("Population Data : ",data)
+        }
+    })
+    .catch((err) => {
+        weatherDetails.innerHTML += '<p>Some Exception Ocured! </p>'
+    })    
+
 
 }
+    
+
